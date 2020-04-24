8376e7df059785f6431cb5fd4bf363392e695f3b
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-24 16:40:11 -0300

    Criacao da classe Individual

---
+++ Exerc_Prop_136/Entities/Class/Individual.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_Prop_136.Entities.Class
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+        public Individual()
+        {
+
+        }
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public sealed override double Tax()
+        {
+            double tax;
+            if(AnualIncome < 20000.00)
+            {
+                tax = AnualIncome * 0.15;
+            }
+            else
+            {
+                tax = AnualIncome * 0.25;
+            }
+
+            if (HealthExpenditures > 0.00)
+            {
+                double abat = HealthExpenditures * 0.5;
+                return AnualIncome - (tax - abat);
+            }
+            else 
+            { 
+                return AnualIncome - tax;
+            }
+        }
+
+    }
+}
