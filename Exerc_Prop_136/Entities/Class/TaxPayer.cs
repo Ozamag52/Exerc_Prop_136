8a2ce266e901b380c992fba7daba73a9393cfc70
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-24 16:28:47 -0300

    Criacao da classe abstrata TaxPayer

---
+++ Exerc_Prop_136/Entities/Class/TaxPayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_Prop_136.Entities.Class
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+    
+        public TaxPayer()
+        {
+
+        }
+        public TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+
+    }
+}
